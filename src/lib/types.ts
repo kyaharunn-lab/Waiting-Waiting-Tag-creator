7ad80f9ca771b026e199c464e4bcdb50696b5f76
Harun Kaya <harungaming16@gmail.com>
2026-05-01 13:54:34 +0000

    Uygulama resetlenince veya ilk açıldığında “DELFİN KOLTUK TAKIMI (3+3+B)

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -22,15 +22,8 @@ export interface LabelData {
 }
 
 export const initialLabelData: LabelData = {
-  productTitle: "DELFİN KOLTUK TAKIMI (3+3+B)",
-  totalCashPrice: "90.000",
-  totalInstallmentPrice: "120.000",
-  tableRows: [
-    {
-      productName: "DELFİN 3'LU KOLTUK",
-      quantity: "1",
-      cashPrice: "90.000",
-      installmentPrice: "120.000",
-    }
-  ],
+  productTitle: "",
+  totalCashPrice: "",
+  totalInstallmentPrice: "",
+  tableRows: [],
 };
